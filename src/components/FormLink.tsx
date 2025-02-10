8651b1e1fb89d95b748550f65bd39aad3bd2f0c2
Stepan JSN <stepan.s.jsn@gmail.com>
2025-02-10 15:34:41 +0200

    Link and FormLink components

---
+++ src/components/FormLink.tsx
@@ -0,0 +1,16 @@
+import { LinkProps, SxProps } from '@mui/material';
+import Link from './Link';
+
+const linkStyles: SxProps = {
+  display: 'block',
+  textAlign: 'center',
+  mt: 1,
+};
+
+export default function FormLink(props: LinkProps & { href: string }) {
+  return (
+    <Link {...props} sx={linkStyles}>
+      {props.children}
+    </Link>
+  );
+}
---
+++ src/components/Link.tsx
@@ -0,0 +1,10 @@
+import { Link as MuiLink, LinkProps } from '@mui/material';
+import NextLink from "next/link";
+
+export default function Link(props: LinkProps & { href: string }) {
+  return (
+    <MuiLink component={NextLink} {...props}>
+      {props.children}
+    </MuiLink>
+  );
+}
