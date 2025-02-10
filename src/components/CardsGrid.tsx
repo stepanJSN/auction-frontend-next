d9dd10c451f979772f18056e1ba9f7e0d428863b
Stepan JSN <stepan.s.jsn@gmail.com>
2025-02-10 14:20:35 +0200

    move CardsGrid component

---
+++ src/components/CardsGrid.tsx
@@ -0,0 +1,29 @@
+import { Grid2 } from '@mui/material';
+import Card from './Card';
+import { ICardSummary } from '../interfaces/cards.interface';
+
+type CardsGridProps = {
+  cards: ICardSummary[];
+  cardPagePath: (cardId: string) => string;
+  cardActions?: (card: ICardSummary) => React.ReactNode;
+};
+
+const cardColumnsNumber = { xs: 12, sm: 6, md: 4, lg: 3 };
+
+export default function CardsGrid({
+  cards,
+  cardActions,
+  cardPagePath,
+}: CardsGridProps) {
+  return (
+    <Grid2 container spacing={2}>
+      {cards.map((card) => (
+        <Grid2 size={cardColumnsNumber} key={card.id}>
+          <Card {...card} cardPagePath={cardPagePath}>
+            {cardActions && cardActions(card)}
+          </Card>
+        </Grid2>
+      ))}
+    </Grid2>
+  );
+}
