aae599444239b8b018222d64eddef594ff8d4386
Stepan JSN <stepan.s.jsn@gmail.com>
2025-02-11 13:19:15 +0200

    clear cookie and call logout service method in logout action

--- src/app/(main)/(header)/logout.action.ts
+++ src/app/(main)/(header)/logout.action.ts
@@ -1,10 +1,14 @@
 "use server";
 
+import { clearStorage } from "@/actions";
 import { ROUTES } from "@/config/routesConfig";
+import { authService } from "@/services/authService";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 
 export async function logoutAction() {
+  await clearStorage();
+  await authService.logout();
   revalidatePath("/");
   redirect(ROUTES.SIGN_IN);
 }
