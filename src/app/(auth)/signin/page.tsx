0a77e5558d6fec06822b809fc9416ee058f3851c
Stepan JSN <stepan.s.jsn@gmail.com>
2025-02-11 09:12:19 +0200

    add signin page

---
+++ src/app/(auth)/signin/page.tsx
@@ -0,0 +1,17 @@
+import { FormWrapper } from '@/components/FormWrapper';
+import AuthForm from './AuthForm';
+import { Typography } from '@mui/material';
+import FormLink from '@/components/FormLink';
+import { ROUTES } from '@/config/routesConfig';
+
+export default function Signin() {
+  return (
+    <FormWrapper>
+      <Typography variant="h5" component="h1" align="center" gutterBottom>
+        Signin
+      </Typography>
+      <AuthForm />
+      <FormLink href={ROUTES.SIGN_UP}>Sign up</FormLink>
+    </FormWrapper>
+  );
+}
