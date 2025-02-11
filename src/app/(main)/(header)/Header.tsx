cf7bba3e3a6b765cfeb9ca81af0ac683d15688ee
Stepan JSN <stepan.s.jsn@gmail.com>
2025-02-11 11:27:22 +0200

    Header component

---
+++ src/app/(main)/(header)/Header.tsx
@@ -0,0 +1,103 @@
+"use client";
+
+import {
+  Button,
+  Grid2,
+  Grid2Props,
+  styled,
+  SxProps,
+  Theme,
+  Typography,
+  useMediaQuery,
+  useTheme,
+} from "@mui/material";
+import Menu from "./Menu";
+import ProfileMenu from "./ProfileMenu";
+import MenuIcon from "@mui/icons-material/Menu";
+import useMenu from "./useMenu";
+import MainContainer from "../MainContainer";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { selectUser } from "@/lib/features/user/userSlice";
+import { Role } from "@/enums/role.enum";
+import { adminMenu, userMenu } from "@/config/menuConfig";
+import { QueryStatusEnum } from "@/enums/queryStatus.enum";
+
+const HeaderStyled = styled(Grid2)<Grid2Props>(({ theme }) => ({
+  position: "relative",
+  padding: theme.spacing(1, 0),
+  [theme.breakpoints.up("lg")]: {
+    padding: theme.spacing(0),
+  },
+}));
+
+const logoStyles: SxProps<Theme> = (theme: Theme) => ({
+  color: theme.palette.common.white,
+  fontWeight: 500,
+  fontSize: {
+    xs: theme.typography.subtitle1.fontSize,
+    md: theme.typography.h6.fontSize,
+  },
+});
+
+const logoGridStyles: Grid2Props = {
+  size: {
+    xs: "grow",
+    md: 3,
+  },
+};
+
+export default function Header() {
+  const { role, status, name, surname, balance, rating } =
+    useSelector(selectUser);
+  const { anchorMenuEl, isMenuOpen, handleMenuClick, handleMenuClose } =
+    useMenu();
+  const theme = useTheme();
+  const isBigScreen = useMediaQuery(theme.breakpoints.up("lg"));
+
+  const menuItems = useMemo(
+    () => (role === Role.USER ? userMenu : adminMenu),
+    [role],
+  );
+
+  return (
+    <MainContainer>
+      <HeaderStyled
+        container
+        component="header"
+        alignItems="center"
+        spacing={2}
+      >
+        <Grid2 {...logoGridStyles}>
+          <Typography sx={logoStyles}>Rick & Morty cards</Typography>
+        </Grid2>
+        {isBigScreen && (
+          <Grid2 size={8}>
+            <Menu menuItems={menuItems} />
+          </Grid2>
+        )}
+        <Grid2 display="flex" justifyContent="end" size="grow">
+          <Button
+            variant="contained"
+            color="secondary"
+            onClick={handleMenuClick}
+          >
+            {isBigScreen ? "Profile" : <MenuIcon />}
+          </Button>
+        </Grid2>
+        <ProfileMenu
+          isMenuOpen={isMenuOpen}
+          handleClose={handleMenuClose}
+          anchorMenuEl={anchorMenuEl}
+          menuItems={menuItems}
+          isUserDataLoaded={status === QueryStatusEnum.SUCCESS}
+          username={`${name} ${surname}`}
+          balance={balance!}
+          rating={rating}
+          isBigScreen={isBigScreen}
+          isAdmin={role === Role.ADMIN}
+        />
+      </HeaderStyled>
+    </MainContainer>
+  );
+}
