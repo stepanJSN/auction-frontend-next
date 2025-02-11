5f86512ac7cf3cd82b0e7b0d51f43bad9d347384
Stepan JSN <stepan.s.jsn@gmail.com>
2025-02-11 15:54:06 +0200

    add NoCards and CardData components

---
+++ src/app/(main)/(mainPage)/CardsData.tsx
@@ -0,0 +1,51 @@
+import { cardsService } from "@/services/cardsService";
+import NoCards from "./NoCards";
+import { ICardSummary } from "@/interfaces/cards.interface";
+import { Button } from "@mui/material";
+import Link from "next/link";
+import { ROUTES } from "@/config/routesConfig";
+import Pagination from "@/components/Pagination";
+import CardsGrid from "@/components/CardsGrid";
+import PageError from "@/components/PageError";
+
+type CardsDataProps = {
+  currentPage: number;
+};
+
+export default async function CardsData({ currentPage }: CardsDataProps) {
+  const { data } = await cardsService.getAll({
+    page: currentPage,
+    onlyUserCards: true,
+  });
+
+  if (!data) {
+    return <PageError />;
+  }
+  if (data.data.length === 0) {
+    return <NoCards />;
+  }
+
+  const cardActions = (card: ICardSummary) => (
+    <>
+      <Button
+        size="small"
+        color="success"
+        component={Link}
+        href={ROUTES.CREATE_AUCTION(card.id)}
+      >
+        Sell
+      </Button>
+    </>
+  );
+
+  return (
+    <>
+      <CardsGrid
+        cards={data.data}
+        cardActions={cardActions}
+        cardPagePath={ROUTES.USER_CARDS_DETAILS}
+      />
+      <Pagination totalPages={data.info.totalPages} />
+    </>
+  );
+}
---
+++ src/app/(main)/(mainPage)/NoCards.tsx
@@ -0,0 +1,31 @@
+import { Stack, Typography, Button, SxProps } from "@mui/material";
+import { ROUTES } from "../../../config/routesConfig";
+import Link from "../../../components/Link";
+
+const containerStyles: SxProps = {
+  alignItems: "center",
+};
+const actionStyles: SxProps = {
+  alignItems: "center",
+  justifyContent: "center",
+};
+export default function NoCards() {
+  return (
+    <Stack sx={containerStyles}>
+      <Typography variant="h5" gutterBottom>
+        You have no cards
+      </Typography>
+      <Stack direction="row" spacing={2} sx={actionStyles}>
+        <Button
+          component={Link}
+          href={ROUTES.AUCTIONS}
+          variant="contained"
+          color="success"
+        >
+          Buy cards on auctions
+        </Button>
+        <Link href={ROUTES.CARDS}>See all cards</Link>
+      </Stack>
+    </Stack>
+  );
+}
