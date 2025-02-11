fd389a68d626cc1aa3cf9032dd17999165f34442
Stepan JSN <stepan.s.jsn@gmail.com>
2025-02-11 09:04:17 +0200

    add auth layout

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,29 @@
+import { Box, SxProps, Theme, Typography } from '@mui/material';
+
+const authContainerStyles: SxProps = {
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  justifyContent: 'center',
+  minHeight: '100vh',
+  padding: 2,
+};
+
+const titleStyles = {
+  mb: {
+    sx: 1,
+    md: 2,
+  },
+  textAlign: 'center',
+};
+
+export default function AuthLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <Box sx={authContainerStyles}>
+      <Typography variant="h4" sx={titleStyles}>
+        Rick & Morty cards
+      </Typography>
+      {children}
+    </Box>
+  );
+}
