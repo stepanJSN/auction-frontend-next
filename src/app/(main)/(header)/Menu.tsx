ebd8e9fbef3fbc5616892923c2a680731a8b61f2
Stepan JSN <stepan.s.jsn@gmail.com>
2025-02-11 11:31:00 +0200

    add Menu and MenuLink

---
+++ src/app/(main)/(header)/Menu.tsx
@@ -0,0 +1,35 @@
+import { List, ListItem, SxProps } from "@mui/material";
+import { MenuLink } from "./MenuLink";
+
+type MenuProps = {
+  menuItems: {
+    label: string;
+    path: string;
+  }[];
+};
+
+const listStyles: SxProps = {
+  display: {
+    xs: "block",
+    lg: "flex",
+  },
+};
+
+const itemStyles: SxProps = {
+  justifyContent: {
+    xs: "initial",
+    lg: "center",
+  },
+};
+
+export default function Menu({ menuItems }: MenuProps) {
+  return (
+    <List sx={listStyles}>
+      {menuItems.map((item) => (
+        <ListItem key={item.label} sx={itemStyles}>
+          <MenuLink href={item.path}>{item.label}</MenuLink>
+        </ListItem>
+      ))}
+    </List>
+  );
+}
---
+++ src/app/(main)/(header)/MenuLink.ts
@@ -0,0 +1,10 @@
+import { styled, LinkProps } from "@mui/material";
+import Link from "@/components/Link";
+
+export const MenuLink = styled(Link)<LinkProps & { href: string }>(
+  ({ theme }) => ({
+    width: "100%",
+    color: theme.palette.common.white,
+    fontWeight: 500,
+  }),
+);
