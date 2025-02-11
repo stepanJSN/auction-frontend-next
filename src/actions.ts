82a03f3122df0b8b97122b9511f947721b35b0d0
Stepan JSN <stepan.s.jsn@gmail.com>
2025-02-11 13:22:52 +0200

    remove template literal

--- src/actions.ts
+++ src/actions.ts
@@ -1,18 +1,19 @@
 "use server";
 
 import { cookies } from "next/headers";
+import { ACCESS_TOKEN_KEY } from "./constants/globalConstants";
 
 export async function setAccessToken(accessToken: string) {
   const cookieStore = await cookies();
-  cookieStore.set("accessToken", accessToken);
+  cookieStore.set(ACCESS_TOKEN_KEY, accessToken);
 }
 
 export async function getAccessToken() {
   const cookieStore = await cookies();
-  return cookieStore.get("accessToken")?.value;
+  return cookieStore.get(ACCESS_TOKEN_KEY)?.value;
 }
 
 export async function clearStorage() {
   const cookieStore = await cookies();
-  cookieStore.delete("accessToken");
+  cookieStore.delete(ACCESS_TOKEN_KEY);
 }
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import { cookies } from "next/headers";
 import { ROUTES } from "@/config/routesConfig";
+import { ACCESS_TOKEN_KEY } from "./constants/globalConstants";
 
 const publicRoutes = [ROUTES.SIGN_IN, ROUTES.SIGN_UP];
 
@@ -8,7 +9,7 @@ export default async function middleware(req: NextRequest) {
   const path = req.nextUrl.pathname;
   const isPublicRoute = publicRoutes.includes(path);
 
-  const cookie = (await cookies()).get("accessToken")?.value;
+  const cookie = (await cookies()).get(ACCESS_TOKEN_KEY)?.value;
 
   if (!isPublicRoute && !cookie) {
     return NextResponse.redirect(new URL(ROUTES.SIGN_IN, req.nextUrl));
