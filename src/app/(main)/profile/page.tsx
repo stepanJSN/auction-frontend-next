f7ac7107a2933f7fd36b09e788796ef8710265a4
Stepan JSN <stepan.s.jsn@gmail.com>
2025-02-11 16:19:49 +0200

    profile page

---
+++ src/app/(main)/profile/page.tsx
@@ -0,0 +1,87 @@
+"use client";
+import { FormWrapper } from "@/components/FormWrapper";
+import { MutationStatusEnum } from "@/enums/mutationStatus";
+import { QueryStatusEnum } from "@/enums/queryStatus.enum";
+import { selectUser } from "@/lib/features/user/userSlice";
+import {
+  Alert,
+  Button,
+  CircularProgress,
+  Stack,
+  SxProps,
+  Typography,
+} from "@mui/material";
+import { useSelector } from "react-redux";
+import useDeleteUser from "./useDeleteUser";
+import useUpdateUser from "./useUpdateUser";
+import UpdateUserForm from "./UpdateUserForm";
+
+const formWrapperStyles: SxProps = {
+  minWidth: "340px",
+  position: "relative",
+};
+
+const alertStyles: SxProps = {
+  position: "absolute",
+  width: "100%",
+  top: "10px",
+  left: "0",
+};
+
+export default function Profile() {
+  const { status, updateStatus, deleteStatus, name, surname, email } =
+    useSelector(selectUser);
+  const { id } = useSelector(selectUser);
+  const handleDelete = useDeleteUser(id!, deleteStatus);
+  const handleUpdate = useUpdateUser(id!);
+
+  return (
+    <Stack alignItems="center" pt={4}>
+      <FormWrapper sx={formWrapperStyles}>
+        <Typography variant="h5" component="h1" align="center" gutterBottom>
+          Profile
+        </Typography>
+        {status === QueryStatusEnum.LOADING && (
+          <Stack alignItems="center" justifyContent="center" minHeight="300px">
+            <CircularProgress />
+          </Stack>
+        )}
+        {updateStatus === MutationStatusEnum.SUCCESS && (
+          <Alert sx={alertStyles} severity="success">
+            The profile was successfully updated
+          </Alert>
+        )}
+        {updateStatus === MutationStatusEnum.ERROR && (
+          <Alert sx={alertStyles} severity="error">
+            An error occurred during the update
+          </Alert>
+        )}
+        {deleteStatus === MutationStatusEnum.ERROR && (
+          <Alert sx={alertStyles} severity="error">
+            An error occurred while deleting
+          </Alert>
+        )}
+        {status === QueryStatusEnum.SUCCESS && (
+          <>
+            <Typography variant="h6">Email: {email}</Typography>
+            <UpdateUserForm
+              isUpdating={updateStatus === MutationStatusEnum.PENDING}
+              onSubmit={handleUpdate}
+              name={name!}
+              surname={surname!}
+            />
+            <Button
+              variant="contained"
+              color="error"
+              fullWidth
+              onClick={handleDelete}
+              disabled={deleteStatus === MutationStatusEnum.PENDING}
+            >
+              Delete profile
+            </Button>
+          </>
+        )}
+      </FormWrapper>
+    </Stack>
+  );
+}
