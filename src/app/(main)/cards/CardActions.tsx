366226cb533ce24393daf124b7de91ba24941dcc
Stepan JSN <stepan.s.jsn@gmail.com>
2025-02-12 13:21:35 +0200

    fix hydration error

--- src/app/(main)/cards/CardActions.tsx
+++ src/app/(main)/cards/CardActions.tsx
@@ -1,36 +1,33 @@
-"use client";
-
 import { ROUTES } from "@/config/routesConfig";
 import { Role } from "@/enums/role.enum";
 import { ICardSummary } from "@/interfaces/cards.interface";
-import { selectUser } from "@/lib/features/user/userSlice";
+import { userService } from "@/services/userService";
 import { Button } from "@mui/material";
 import Link from "next/link";
-import { useSelector } from "react-redux";
 
 interface CardActionsProps {
   card: ICardSummary;
 }
 
-export default function CardActions({ card }: CardActionsProps) {
-  const { role } = useSelector(selectUser);
-
-  if (role !== Role.ADMIN) return null;
+export default async function CardActions({ card }: CardActionsProps) {
+  const { role } = await userService.getCurrent();
 
   return (
-    <>
-      <Button
-        size="small"
-        color="success"
-        component={Link}
-        disabled={!card.is_active}
-        href={ROUTES.CREATE_AUCTION(card.id)}
-      >
-        Sell
-      </Button>
-      <Button component={Link} href={ROUTES.EDIT_CARD(card.id)} size="small">
-        Manage
-      </Button>
-    </>
+    role === Role.ADMIN && (
+      <>
+        <Button
+          size="small"
+          color="success"
+          component={Link}
+          disabled={!card.is_active}
+          href={ROUTES.CREATE_AUCTION(card.id)}
+        >
+          Sell
+        </Button>
+        <Button component={Link} href={ROUTES.EDIT_CARD(card.id)} size="small">
+          Manage
+        </Button>
+      </>
+    )
   );
 }
