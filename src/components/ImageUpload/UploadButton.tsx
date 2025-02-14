1fc83850a2535e568ac1e2a7de9021f550ffb502
Stepan JSN <stepan.s.jsn@gmail.com>
2025-02-14 12:25:25 +0200

    format UploadButton component, add CardFormInputs and EpisodesListFormComponents

--- src/components/ImageUpload/UploadButton.tsx
+++ src/components/ImageUpload/UploadButton.tsx
@@ -1,4 +1,4 @@
-import { Button, CircularProgress, styled, SxProps } from '@mui/material';
+import { Button, CircularProgress, styled, SxProps } from "@mui/material";
 
 type UploadButtonProps = {
   handleUpload: (e: React.ChangeEvent<HTMLInputElement>) => void;
@@ -6,21 +6,21 @@ type UploadButtonProps = {
   isError: boolean;
 };
 
-const VisuallyHiddenInput = styled('input')({
-  clip: 'rect(0 0 0 0)',
-  clipPath: 'inset(50%)',
+const VisuallyHiddenInput = styled("input")({
+  clip: "rect(0 0 0 0)",
+  clipPath: "inset(50%)",
   height: 1,
-  overflow: 'hidden',
-  position: 'absolute',
+  overflow: "hidden",
+  position: "absolute",
   bottom: 0,
   left: 0,
-  whiteSpace: 'nowrap',
+  whiteSpace: "nowrap",
   width: 1,
 });
 
 const buttonStyles: SxProps = {
-  height: '100%',
-  width: '100%',
+  height: "100%",
+  width: "100%",
 };
 
 export default function UploadButton({
@@ -35,9 +35,10 @@ export default function UploadButton({
         variant="outlined"
         tabIndex={-1}
         disabled={isLoading}
-        color={isError ? 'error' : 'primary'}
-        sx={buttonStyles}>
-        {isLoading ? <CircularProgress /> : 'Upload image'}
+        color={isError ? "error" : "primary"}
+        sx={buttonStyles}
+      >
+        {isLoading ? <CircularProgress /> : "Upload image"}
         <VisuallyHiddenInput
           type="file"
           onChange={handleUpload}
