1f2d4f80b603657e254c87900cca4bea1c1f44f7
Stepan JSN <stepan.s.jsn@gmail.com>
2025-02-14 15:28:29 +0200

    add delete and update card actions

--- src/app/(main)/cards/cards.actions.ts
+++ src/app/(main)/cards/cards.actions.ts
@@ -2,8 +2,12 @@
 
 import { episodesService } from "@/services/episodesService";
 import { locationsService } from "@/services/locationsService";
-import { ICardFrom } from "./create/CreateCardForm";
+import { ICardFrom } from "@/interfaces/cards.interface";
 import { cardsService } from "@/services/cardsService";
+import { redirect } from "next/navigation";
+import { ROUTES } from "@/config/routesConfig";
+import { MutationStatusEnum } from "@/enums/mutationStatus";
+import { revalidatePath } from "next/cache";
 
 export async function getEpisodesByNameAction(episodeName: string) {
   return episodesService.getAll({ name: episodeName });
@@ -21,4 +25,22 @@ export async function createCardAction(data: ICardFrom) {
     locationId: data.location.id,
     episodesId: data.episodes.map((episode) => episode.id),
   });
+}
+
+export async function editCardAction(id: string, data: ICardFrom) {
+  return cardsService.update(id, {
+    ...data,
+    image: data.image?.image,
+    locationId: data.location.id,
+    episodesId: data.episodes.map((episode) => episode.id),
+  });
+}
+
+export async function deleteCardAction(id: string) {
+  const { status } = await cardsService.delete(id);
+  if (status === MutationStatusEnum.ERROR) {
+    return status;
+  }
+  revalidatePath(ROUTES.CARDS);
+  redirect(ROUTES.CARDS);
 }
