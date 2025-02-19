025ed03611cb75dc3ed3ca5eeaa6179f66919e09
Stepan JSN <stepan.s.jsn@gmail.com>
2025-02-19 17:31:42 +0200

    add error interceptors to system and transaction service

--- src/services/transactionsService.ts
+++ src/services/transactionsService.ts
@@ -1,24 +1,40 @@
-import { cache } from 'react';
-import { apiWithAuth } from '../apiConfig';
-import { IBalance } from '../interfaces/user.interfaces';
+import { cache } from "react";
+import { apiWithAuth } from "../apiConfig";
+import { IBalance } from "../interfaces/user.interfaces";
+import { MutationStatusEnum } from "@/enums/mutationStatus";
 
 export const transactionsService = {
   topUp: async (amount: number) => {
-    const balance = await apiWithAuth.post<IBalance>('/transactions/topUp', {
-      amount,
-    });
-    return balance.data;
+    try {
+      const balance = await apiWithAuth.post<IBalance>("/transactions/topUp", {
+        amount,
+      });
+      return { data: balance.data, status: MutationStatusEnum.SUCCESS };
+    } catch {
+      return { status: MutationStatusEnum.ERROR };
+    }
   },
   withdraw: async (amount: number) => {
-    const balance = await apiWithAuth.post<IBalance>('/transactions/withdraw', {
-      amount,
-    });
-    return balance.data;
+    try {
+      const balance = await apiWithAuth.post<IBalance>(
+        "/transactions/withdraw",
+        {
+          amount,
+        },
+      );
+      return { data: balance.data, status: MutationStatusEnum.SUCCESS };
+    } catch {
+      return { status: MutationStatusEnum.ERROR };
+    }
   },
   getFeeAmount: cache(async () => {
-    const fee = await apiWithAuth.get<{ totalFeeAmount: number }>(
-      '/transactions/fee',
-    );
-    return fee.data;
+    try {
+      const fee = await apiWithAuth.get<{ totalFeeAmount: number }>(
+        "/transactions/fee",
+      );
+      return { data: fee.data, status: MutationStatusEnum.SUCCESS };
+    } catch {
+      return { status: MutationStatusEnum.ERROR };
+    }
   }),
 };
