c3deee64ceef7f80ef91b2f5382e2a2eda0e3e79
Stepan JSN <stepan.s.jsn@gmail.com>
2025-02-20 12:19:45 +0200

    move business logic into hooks

--- src/app/(main)/transactions/_components/TransactionForms.tsx
+++ src/app/(main)/transactions/_components/TransactionForms.tsx
@@ -1,19 +1,15 @@
 "use client";
 import { Grid2, GridSize } from "@mui/material";
-import { topUpAction, withdrawAction } from "../transactions.actions";
 import TransactionForm from "./TransactionForm";
-import { useCallback, useMemo, useState } from "react";
+import { useMemo } from "react";
 import { Role } from "@/enums/role.enum";
-import usePaymentIntent from "../usePaymentIntent";
 import { loadStripe } from "@stripe/stripe-js";
 import { Elements } from "@stripe/react-stripe-js";
 import { STRIPE_PUBLISHABLE_KEY } from "@/constants/envConstants";
 import PaymentForm from "./PaymentForm";
-import { MutationStatusEnum } from "@/enums/mutationStatus";
 import WithdrawError from "./WithdrawError";
-import { useSnackbar } from "notistack";
-import { useDispatch } from "react-redux";
-import { updateUserBalance } from "@/lib/features/user/userSlice";
+import useTopUp from "../useTopUp";
+import useWithdraw from "../useWithdraw";
 
 const gridFormColumns: Record<string, GridSize> = {
   xs: 12,
@@ -32,44 +28,9 @@ export default function TransactionForms({
   role,
   exchangeRate,
 }: TransactionFormsProps) {
-  const { handleTopUp, paymentData, resetPaymentData } = usePaymentIntent();
-  const [withdrawError, setWithdrawError] = useState<number | null>(null);
-  const { enqueueSnackbar } = useSnackbar();
-  const dispatch = useDispatch();
+  const { onTopUpSubmit, paymentData, resetPaymentData } = useTopUp(role);
+  const { onWithdrawSubmit, withdrawError } = useWithdraw();
 
-  const onTopUpSubmit = useCallback(
-    async (amount: number) => {
-      if (role === Role.ADMIN) {
-        const response = await topUpAction(amount);
-        if (response.status === MutationStatusEnum.SUCCESS && response.data) {
-          enqueueSnackbar("Operation was successfully", {
-            variant: "success",
-          });
-          dispatch(updateUserBalance(response.data));
-          return response.status;
-        }
-        enqueueSnackbar("Error. Operation was not successful", {
-          variant: "error",
-        });
-      }
-      handleTopUp(amount);
-    },
-    [role, handleTopUp, enqueueSnackbar, dispatch],
-  );
-  const onWithdrawSubmit = useCallback(
-    async (amount: number) => {
-      const response = await withdrawAction(amount);
-      if (response.status === MutationStatusEnum.SUCCESS && response.data) {
-        enqueueSnackbar("Operation was successfully", {
-          variant: "success",
-        });
-        dispatch(updateUserBalance(response.data));
-        return response.status;
-      }
-      setWithdrawError(response.errorCode);
-    },
-    [dispatch, enqueueSnackbar],
-  );
   const stripeOptions = useMemo(
     () => ({ clientSecret: paymentData?.clientSecret }),
     [paymentData?.clientSecret],
