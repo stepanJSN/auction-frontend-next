ed2f2e844ad8f379b3f5407dc1546aff12fb93a5
Stepan JSN <stepan.s.jsn@gmail.com>
2025-02-21 15:54:42 +0200

    chat page without settings

--- src/app/(main)/chats/[id]/page.tsx
+++ src/app/(main)/chats/[id]/page.tsx
@@ -1,5 +1,42 @@
+import PageError from "@/components/PageError";
+import { chatsService } from "@/services/chatsService";
+import { Grid2 } from "@mui/material";
 import React from "react";
+import ChatHeader from "./_components/ChatHeader";
+import ChatField from "./_components/ChatField";
 
-export default function ChatPage() {
-  return <div>chatPage</div>;
+export default async function ChatPage({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const id = (await params).id;
+  const [{ data: messages }, { data: chatData }] = await Promise.all([
+    chatsService.findAllMessages({
+      id,
+    }),
+    chatsService.findOne(id),
+  ]);
+
+  if (!messages || !chatData) {
+    return <PageError />;
+  }
+
+  return (
+    <Grid2 container spacing={1}>
+      <Grid2 size="grow">
+        <>
+          <ChatHeader
+            name={chatData.name}
+            numberOfParticipants={chatData.users.length}
+          />
+          <ChatField
+            chatId={id}
+            initialCursor={messages.pagination.cursor}
+            initialMessages={messages.data}
+          />
+        </>
+      </Grid2>
+    </Grid2>
+  );
 }
