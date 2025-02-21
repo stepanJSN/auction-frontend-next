57dba0a81167c5368630f48e460e57a0e9b59179
Stepan JSN <stepan.s.jsn@gmail.com>
2025-02-21 12:15:17 +0200

    create chat server action

--- src/app/(main)/chats/chats.actions.ts
+++ src/app/(main)/chats/chats.actions.ts
@@ -1,10 +1,22 @@
 "use server";
 
+import { ROUTES } from "@/config/routesConfig";
+import { MutationStatusEnum } from "@/enums/mutationStatus";
+import { ICreateChat } from "@/interfaces/chats.interfaces";
 import { chatsService } from "@/services/chatsService";
+import { redirect } from "next/navigation";
 
 export async function getMoreChats(page: number, chatName?: string) {
   return chatsService.findAll({
     page,
     name: chatName,
   });
+}
+
+export async function createChatAction(data: ICreateChat) {
+  const response = await chatsService.create(data);
+  if (response.status === MutationStatusEnum.SUCCESS && response.data) {
+    redirect(ROUTES.CHAT(response.data.id));
+  }
+  return response;
 }
